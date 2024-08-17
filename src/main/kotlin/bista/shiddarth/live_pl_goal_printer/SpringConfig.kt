3c13eb8be14ed6e92c75c48011ae81bcec9bbac7
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2024-08-17 17:58:51 -0400

    Implement scheduling

---
+++ src/main/kotlin/bista/shiddarth/live_pl_goal_printer/SpringConfig.kt
@@ -0,0 +1,9 @@
+package bista.shiddarth.live_pl_goal_printer
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SpringConfig {
+}
--- src/main/kotlin/bista/shiddarth/live_pl_goal_printer/controller/PrinterController.kt
+++ src/main/kotlin/bista/shiddarth/live_pl_goal_printer/controller/PrinterController.kt
@@ -2,23 +2,38 @@ package bista.shiddarth.live_pl_goal_printer.controller
 
 import bista.shiddarth.live_pl_goal_printer.model.FplEvent
 import bista.shiddarth.live_pl_goal_printer.model.GoalEvent
+import bista.shiddarth.live_pl_goal_printer.model.PlayerMap
 import bista.shiddarth.live_pl_goal_printer.service.DataFetcherService
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestClient
 
 @RestController
 class PrinterController(private val dataFetcherService: DataFetcherService) {
-    val restClient = RestClient().create
+    private final val restClient: RestClient = RestClient.create()
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
+    @Scheduled(fixedDelay = 60000)
     @GetMapping("/goals")
     fun getGoalScored(): List<GoalEvent> {
-        return dataFetcherService.getGoalsScored()
+        val goalsScored = dataFetcherService.getGoalsScored()
+        logger.info(goalsScored.toString())
+        return goalsScored
     }
 
     @GetMapping("/players")
-    fun getPlayers() {
+    fun getPlayers() : List<String> {
+        println("CALLING ")
+        val result = restClient.get()
+            .uri("https://fantasy.premierleague.com/api/bootstrap-static/")
+            .retrieve()
+            .body(PlayerMap::class.java)
 
+        return result?.elements?.map {
+            "${it.id} to ${it.firstName} ${it.lastName}"
+        } ?: emptyList()
     }
 
 }
